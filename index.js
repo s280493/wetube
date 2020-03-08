6ede07e3e0ebd2a42626690edb1545a1b011f2e0
cien.lee <cien.lee@kakaopaycorp.com>
2020-03-08 23:03:11 +0900

    미들웨어 설치

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-const app = express() // 가져온 express를 실행하여 express application을 만든다
-const PORT = 3000
+const app = express(); // 가져온 express를 실행하여 express application을 만든다
+const PORT = 3000;
 
 const handleListening = () => console.log(`app listening on port ${PORT}!`)
 
@@ -9,15 +13,16 @@ const handleHome = (req, res) => res.send('Hello APP!');
 
 const handleProfile = (req,res) => res.send("You are on my profile!");
 
-const betweenHome = (req, res, next) => {
-  console.log('bewteen')
-  next(); // 다음 미들웨어를 실행해준다!
-}
+app.use(morgan("dev"))
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(betweenHome);
+const middleware = (req, res, next) => res.send('not happening');
 
-app.get('/', (req, res) => handleHome(req, res))
+app.get('/', middleware, handleHome);
 
 app.get('/profile', (req, res) => handleProfile(req, res))
 
-app.listen(PORT, handleListening) // 우리가 만든 app에 
+app.listen(PORT, handleListening)
